7db08f0672cdf0649bc9187baea3e33cc70ebe94
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-03-18 08:06:10 -0700

    Update gitignore

--- LoadingScreenSkip/obj/Debug/net472/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace LoadingScreenSkip
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "LoadingScreenSkip";
-        public const string PLUGIN_NAME = "LoadingScreenSkip";
-        public const string PLUGIN_VERSION = "0.0.2";
-    }
-}
