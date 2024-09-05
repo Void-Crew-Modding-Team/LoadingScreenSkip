76f01a9a2e8fa9271615be3bde6cb414fcd2463f
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-09-05 16:08:34 -0700

    Attempt to ignore build obj files.

--- LoadingScreenSkip/obj/Release/net472/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace LoadingScreenSkip
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "LoadingScreenSkip";
-        public const string PLUGIN_NAME = "LoadingScreenSkip";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
